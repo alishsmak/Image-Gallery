e8455ff133b613aa42309cdd294f33f427984040
alishsmak <alishsmak@gmail.com>
2023-10-28 02:28:32 +0600

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.querySelectorAll('.img-list img').forEach(image =>{
+//     image.onclick = () =>{
+//         document.querySelector('.picture').style.display = 'block';
+//         document.querySelector('.picture img').src = image.getAttribute('src');
+//     }
+// })
+
+// document.querySelector('.picture span').onclick = () =>{
+//     document.querySelector('.picture').style.display = 'none';
+// }
